f205926b3c206f406c058c63de8bd98eb6036cae
bipinchaudhari <bipin.chaudhari@aressindia.net>
2020-03-13 10:59:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/heady/ecommerce/view/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.heady.ecommerce.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heady.ecommerce.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
